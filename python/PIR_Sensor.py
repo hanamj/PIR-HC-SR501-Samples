1c1242c2d39967cfd5d1699594311b308d7b9d03
John Hanam <hanamj@shaw.ca>
2018-11-23 23:55:38 +0000

    Updating python script

--- python/PIR_Sensor.py
+++ python/PIR_Sensor.py
@@ -8,14 +8,20 @@ class PIR():
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
 
-    def inAlarm(self):
+    def getState(self):
         GPIO.setup(self.PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         readVal = GPIO.input(self.PIN)
-        return readVal
+	return readVal
 
 if __name__ == '__main__':
     p = PIR()
+    val = 0
     while True:
-        value = p.inAlarm()
-        print value
-        sleep(1)
+	newval = p.getState()
+	if newval != val:
+	    val = newval
+	    if val == 1:
+	        print "MOTION DETECTED!!"
+	    else:
+	        print "...all quiet now..."
+        sleep(0.25)
